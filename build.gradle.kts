037d611ec67ddf0c02370aa546170627b5e716ff
Kanake <10kanake@gmail.com>
2024-12-04 03:51:43 +0300

    ktlint

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,33 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
-//buildscript {
-//    dependencies {
-//        classpath ("com.google.dagger:hilt-android-gradle-plugin:2.44")
-//    }
-//}
 plugins {
     id ("com.google.dagger.hilt.android") version "2.48.1" apply false
     id("com.google.devtools.ksp") version "1.9.20-1.0.14" apply false
     id("com.android.application") version "8.5.2" apply false
     id("com.android.library") version "8.5.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+    id("org.jlleitschuh.gradle.ktlint") version "10.1.0"
+    id("com.diffplug.spotless") version "5.17.1"
+}
+
+subprojects {
+
+    apply(plugin = "com.diffplug.spotless")
+    spotless {
+        kotlin {
+            target("**/*.kt")
+            licenseHeaderFile(
+                rootProject.file("${project.rootDir}/spotless/copyright.kt"),
+                "^(package|object|import|interface)"
+            )
+        }
+    }
+
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
+    ktlint {
+        android.set(true)
+        verbose.set(true)
+        filter {
+            exclude { element -> element.file.path.contains("generated/") }
+        }
+    }
 }
